351c41729d8f28744648e789ec0977362572cd49
oscar <ojimenezrodriguez@ithaca.edu>
2024-08-31 18:01:07 -0400

    added triangle constructor

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -1,5 +1,11 @@
 package edu.ithaca.dragon.shapes;
 
 public class Triangle {
-    
+    private double base;
+    private double height;
+
+    public Triangle(double b, double h){
+        base = b;
+        height = h;
+    }
 }
