67fb672e1165cb31559cbd0430590c7631b90fe2
oscar <ojimenezrodriguez@ithaca.edu>
2024-09-03 13:30:22 -0400

    added comments to first tests

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -1,22 +1,22 @@
 package edu.ithaca.dragon.practice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 
 public class FunctionPracticeTest {
 
     @Test
     public void largestOfThreeTest(){
+        // This so that it doesn't matter which order the numbers are put in
         assertEquals(5, FunctionPractice.largestOfThree(4, 5, 2));
         assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
         assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
+        // Same number three times
         assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
 
+        // Checks that throws error if any (or all) arguments are negative
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
