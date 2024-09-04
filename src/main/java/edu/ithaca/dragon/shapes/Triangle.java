5e0aa9c02489ad212cc6ebdf9e8ba0d3eca9609f
oscar <ojimenezrodriguez@ithaca.edu>
2024-09-04 09:45:38 -0400

    added toString for Triangle

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -10,17 +10,25 @@ public class Triangle implements Shape {
         height = h;
     }
 
+    @Override
     public double calcArea(){
         return base * height / 2;
     }
 
+    @Override
     public void doubleSize(){
         base = base*2;
         height = height*2;
     }
 
+    @Override
     public double longestLineWithin(){
         // Returns longest side(s)
         return Math.sqrt(Math.pow((base/2),2) + Math.pow(height,2));
+    }
+
+    @Override
+    public String toString(){
+        return "A Triangle with base " + base + " and height " + height;
     }
 }
